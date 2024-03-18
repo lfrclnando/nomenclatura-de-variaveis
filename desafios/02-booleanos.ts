b886956f7521142765cd98ed0f4712ac2ec1f2cf
“lfrclnando” <lfernando.div@gmail.com>
2024-03-18 00:16:22 -0300

    Feat/Renaming Booleans variables

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -10,21 +10,21 @@ const necessaryHeight = 130
 
 const currentHour = new Date().getHours()
 
-const parkOpen = currentHour > 9 && currentHour < 18
+const isParkOpen = currentHour > 9 && currentHour < 18
 
-if (!parkOpen) {
+if (!isParkOpen) {
   throw new Error('O parque está fechado!')
 }
 
-const ticket = user.hasTicket
+const hasTicket = user.hasTicket
 
-if (!ticket) {
+if (!hasTicket) {
   throw new Error('O Diego não possui um bilhete para entrar no parque!')
 }
 
-const enterToy = user.height > necessaryHeight
+const hasEnterToy = user.height > necessaryHeight
 
-if (!enterToy) {
+if (!hasEnterToy) {
   throw new Error('O Diego não pode entrar no brinquedo!')
 } 
 
